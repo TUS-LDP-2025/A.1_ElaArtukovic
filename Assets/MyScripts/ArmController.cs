998ac1f091db774d21099fc23e6af1951fc2feb5
ela-artukovic-tus23 <K00299459@student.tus.ie>
2025-10-15 18:16:05 +0100

    save

---
+++ Assets/MyScripts/ArmController.cs
@@ -0,0 +1,49 @@
+using TMPro;
+using Unity.Burst.Intrinsics;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ArmController : MonoBehaviour
+{
+
+    public float raiseSpeed;
+    private Quaternion startRotation;                 //start rotation
+    private Quaternion endRotation;                    //when raised at end angle
+    public bool isRaising = false;                    //checking if its being raised
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startRotation = transform.localRotation;                      //keep track of how i stored the rotation at start
+        endRotation = Quaternion.Euler(-90f, 0, 0) * startRotation;                   //makes rotation i need of 90 degrees on x
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isRaising = Mouse.current.rightButton.isPressed;                       //check if its pressed, if yes then it can raise it
+
+        if (isRaising)
+        {
+            RaiseArm();
+        }
+
+        else
+        {
+            LowerArm();
+        }
+
+    }
+
+    private void RaiseArm()
+    {
+
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, endRotation, Time.deltaTime * raiseSpeed);      //go from start rot to end rot during the speed i set
+    }
+
+    private void LowerArm()
+    {
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, startRotation, Time.deltaTime * raiseSpeed);          //the same just backwards
+
+    }
+}
---
+++ Assets/MyScripts/Grab.cs
@@ -0,0 +1,80 @@
+using StarterAssets;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+
+public class PickUp : MonoBehaviour
+{
+    public FirstPersonController FirstPersonController;
+    public ArmController armController;
+    public Transform handTransform;
+    public float grabRadius;
+    private GameObject grabbedObject = null;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (armController == null)
+        {
+            armController = GetComponent<ArmController>();                       //add as safety measure if its not set in inspector
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (armController == null || handTransform == null)                    //add null cuz im dumb so i gotta notice if its not actually assigned
+        {
+            return;
+        }
+
+        if (armController.isRaising)              //reference arm controller cuz i gotta know when its actually up to be able to grab, dont grab just when close
+        {
+            if (grabbedObject == null)               //grab only if i didnt already grab something
+            {
+                GrabObject();
+            }
+        }
+
+        else
+        {
+            if (grabbedObject != null)                   //drop object if arm isnt raised anymore and if we are still holding it
+            {
+                ReleaseObject();
+            }
+        }
+    }
+
+    void GrabObject()
+    {
+        Collider[] hits = Physics.OverlapSphere(handTransform.position, grabRadius);                                      //get the collider thats within the collider i set around arm transform
+
+        foreach (Collider hit in hits)
+        {
+            if (hit.CompareTag("Grabbable"))                                                  //only grab grabbable
+            {
+                grabbedObject = hit.gameObject;                                              //i think this stores the grabbed object, ask john or google
+
+                Rigidbody rb = grabbedObject.GetComponent<Rigidbody>();                      //stop it reacting weird to the physics whil i hold it      
+
+                if (rb != null) rb.isKinematic = true;
+
+                grabbedObject.transform.SetParent(handTransform);                          //parent to hand transform so it can move with arm
+                grabbedObject.transform.localPosition = Vector3.zero;                      //it goes to centre of hand, avoid weird snaps
+                grabbedObject.transform.localRotation = Quaternion.identity;                    //makes the rotation match the hand so it doesn't overlap
+                break;
+            }
+        }
+    }
+
+    void ReleaseObject()
+    {
+        Rigidbody rb = grabbedObject.GetComponent<Rigidbody>();
+        if (rb != null) rb.isKinematic = false;                                          //make physics normal again so it can fall when released
+        {
+            grabbedObject.transform.SetParent(null);
+            grabbedObject = null;                                          //shouldn't be attached to parent anymore since its dropped
+        }
+    }
+
+}
